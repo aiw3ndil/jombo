66c37c966eb0850c3e665722b5d226d4988e07a7
Albert Oliva <albert.oliva@gmail.com>
2023-01-21 16:00:13 +0100

    fix routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,15 @@
 class HomeController < ApplicationController
   def index
     @title = '¡Bienvenido a jombo!'
+
+    if user_signed_in?
+      render "dashboard"
+    else
+      render "index"
+    end
+  end
+
+  def dashboard
+    @title = 'Dashboard'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,14 +18,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: 'home#index'
-
-  devise_scope :users do
-    authenticated :users do
-      root 'home#index', as: :authenticated_root
-    end
-
-    unauthenticated do
-      root 'home#index', as: :unauthenticated_root
-    end
-  end
+  
 end
