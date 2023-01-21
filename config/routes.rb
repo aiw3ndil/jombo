5fa6f35033fa9bd1f098d983f69e4fb8bd1ad6b9
Albert Oliva <albert.oliva@gmail.com>
2023-01-21 14:30:35 +0100

    input size and routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: 'home#index'
+
+  devise_scope :users do
+    authenticated :users do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'home#index', as: :unauthenticated_root
+    end
+  end
 end
