9cf2a0c0599ba8abe390bb928284abbf356229ad
Albert Oliva <albert.oliva@gmail.com>
2023-01-21 21:36:29 +0100

    avatar in header and profile edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,29 +4,13 @@ class ProfilesController < ApplicationController
   before_action :set_profile, only: %i[show edit update destroy]
 
   # GET /profiles/1 or /profiles/1.json
-  def show; end
-
-  # GET /profiles/new
-  def new
-    @profile = Profile.new
+  def show
+    @title = 'Perfil'
   end
 
   # GET /profiles/1/edit
-  def edit; end
-
-  # POST /profiles or /profiles.json
-  def create
-    @profile = Profile.new(profile_params)
-
-    respond_to do |format|
-      if @profile.save
-        format.html { redirect_to profile_url(@profile), notice: 'Profile was successfully created.' }
-        format.json { render :show, status: :created, location: @profile }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
-    end
+  def edit
+    @title = "Editar perfil"
   end
 
   # PATCH/PUT /profiles/1 or /profiles/1.json
@@ -62,6 +46,6 @@ class ProfilesController < ApplicationController
   # Only allow a list of trusted parameters through.
   def profile_params
     params.require(:profile).permit(:username, :first_name, :last_name, :street, :city, :region, :postal_code,
-                                    :country, :user_id)
+                                    :country, :user_id, :avatar)
   end
 end
