2c5fc4360869e25253f6da322a76bd6af519f9da
Albert Oliva <albert.oliva@gmail.com>
2023-01-28 17:19:09 +0100

    heder mobile

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -2,4 +2,8 @@ document.addEventListener("turbo:load", function() {
   $('#user-menu-button').click(function() {
     $('#dropdown-user-menu').toggleClass('hidden')
   });
+
+  $('#mobile-menu').click(function() {
+    $('#mobile-menu-items').toggleClass('hidden')
+  });
 })
