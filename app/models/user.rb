bf215d5e1605ffc47baaa759cf544f4dae6db8ea
Albert Oliva <albert.oliva@gmail.com>
2023-02-02 20:43:28 +0100

    try auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,13 +48,13 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :profile
 
-  def self.create_from_provider_data(auth)
+  def self.from_omniauth(auth)
     name_split = auth.info.name.split
     user = User.where(email: auth.info.email).first
     user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email,
                           password: Devise.friendly_token[0, 20])
-    #user.profile = Profile.update(first_name: name_split[1], last_name: name_split[0])
-    #image = URI.parse(auth.info.image).open
-    #user.profile.avatar.attach(io: image, filename: "foo.jpg")
+    user.profile = Profile.update(first_name: name_split[1], last_name: name_split[0])
+    image = URI.parse(auth.info.image).open
+    user.profile.avatar.attach(io: image, filename: "foo.jpg")
   end
 end
