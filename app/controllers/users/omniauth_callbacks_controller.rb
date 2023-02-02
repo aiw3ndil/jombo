1f07c28a944216b7ad617d3a91a3b5cdf048647e
Albert Oliva <albert.oliva@gmail.com>
2023-02-02 21:13:28 +0100

    fix auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,13 +3,15 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env['omniauth.auth'])
+
       if @user.persisted?
-        sign_in_and_redirect @user
-        set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, event: :authentication
       else
-        flash[:error]='Ha habido un problema iniciando sesión desde Google. Por favor registrate o inténtalo de nuevo.'
-        redirect_to new_user_session_path
+        session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
       end
     end
 
